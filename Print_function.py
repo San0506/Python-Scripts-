d099a0d26b6e72c3e3d5bb18a436d8f80e3caad6
Sankalp Jain <43677952+San0506@users.noreply.github.com>
2019-09-06 23:26:30 -0500

    Add files via upload

---
+++ Print_function.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  6 23:25:41 2019
+
+@author: sanjain6
+"""
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1, n+1, 1):
+        print(i, end = '')
